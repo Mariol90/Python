16151f3fb498c339f39a3f89228d183da7cc46e4
Mariol90 <97477260+Mariol90@users.noreply.github.com>
2022-06-09 18:02:39 -0400

    Add files via upload

---
+++ python-syntax/any7.py
@@ -0,0 +1,15 @@
+def any7(nums):
+    """Are any of these numbers a 7? (True/False)"""
+    for num in nums:
+        if num == 7:
+            return 'True there is a 7 here'
+        else:
+            return 'False there is not a 7 here'
+
+    # YOUR CODE HERE
+# print("should be true", any7([1, 2, 7, 4, 5]))
+# print("should be false", any7([1, 2, 4, 5]))
+
+
+
+
