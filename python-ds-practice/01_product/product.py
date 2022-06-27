f6c38dddf66aa422a0f17bbe2c0d65c0506812dc
Mariol90 <97477260+Mariol90@users.noreply.github.com>
2022-06-27 15:43:01 -0400

    Add files via upload

---
+++ python-ds-practice/01_product/product.py
@@ -0,0 +1,16 @@
+def product(a, b):
+    """Return product of a and b.
+
+        >>> product(2, 2)
+        4
+
+        >>> product(2, -2)
+        -4
+    """
+    pro = a * b
+    return pro
+
+product(2, -2)  
+    
+    
+   
---
+++ python-ds-practice/26_vowel_count/vowel_count.py
@@ -0,0 +1,22 @@
+
+
+
+def vowel_count(phrase):
+    """Return frequency map of vowels, case-insensitive.
+
+        >>> vowel_count('rithm school')
+        {'i': 1, 'o': 2}
+        
+        >>> vowel_count('HOW ARE YOU? i am great!') 
+        {'o': 2, 'a': 3, 'e': 2, 'u': 1, 'i': 1}
+    """
+    phrase = phrase.lower()
+    counter ={}
+    VOWELS = set("aeiou")
+
+    for lrt in phrase:
+        if lrt in VOWELS:
+            counter[lrt] = counter.get(lrt, 0) + 1
+
+    return counter
+vowel_count('HOW ARE YOU? i am great!')
